9f3c961a5fb95be982d23054035b0c94842366e6
Rodion <rakcheev012@gmail.com>
2026-01-31 10:48:58 +0300

    added game and classes

---
+++ classes.py
@@ -0,0 +1,211 @@
+from time import sleep
+
+
+
+class Character():
+    def __init__(self, name, hp, weapon, behavior, armor):
+        self.name = name
+        self.hp = hp
+        self.weapon = weapon
+        self.behavior = behavior
+        self.armor = armor
+
+    def deal_hit(self, enemy):
+        enemy.hp -= self.weapon.damage * ((100 - enemy.armor.resist) / 100) 
+        if enemy.hp < 0:
+            enemy.hp = 0
+        print(f'{self.name} нанес урон {enemy.name} равный {self.weapon.damage} \nтеперь {enemy.name} имеет {enemy.hp}')
+
+class Player(Character):
+    def __init__(self, name, hp, weapon, behavior, armor, money, inventory = []):
+        super().__init__(name, hp, weapon, behavior, armor)
+        self.money = money
+        self.inventory = inventory
+    
+    def show_inventory(self):
+        counter = 1
+        print('Инвентарь игрока:')
+        for i in self.inventory:
+            print(f'{counter}  -  {i.name}')
+            counter += 1
+        print(f'\nЭкипировка игрока: \n оружие = {self.weapon.name}\n броня = {self.armor.name}')
+        
+
+    def player_manager(self):
+        while True:
+            self.show_inventory()
+            choose = int(input(f'1 - заменить предмет\n2 - выход\n'))
+            if choose == 1:
+                a = int(input(f'номер предмета:')) -1
+                self.change(a)
+            elif choose == 2:
+                break
+            else:
+                pass
+
+    def change(self, item):
+        while True:
+            if type(self.inventory[item]) == Weapon:
+                self.inventory.append(self.weapon)
+                self.weapon = self.inventory[item]
+                self.inventory.pop(item)
+                break
+            elif type(self.inventory[item]) == Armor:
+                self.inventory.append(self.armor)
+                self.armor = self.inventory[item]
+                self.inventory.pop(item)
+                break
+            elif type(self.inventory[item]) == Heal_potion:
+                self.hp += item.hp
+                self.inventory.pop(item)
+            else:
+                pass
+        
+
+class Weapon():
+    def __init__(self, name, damage, cost):
+        self.name = name
+        self.damage = damage
+        self.cost = cost
+
+    def show_characteristics(self):
+        return f'{self.name},  cost = {self.cost}, damage = {self.damage}'
+    
+
+class Armor():
+    def __init__(self, name, resist, cost, hp):
+        self.name = name
+        self.resist = resist
+        self.cost = cost
+        self.hp = hp
+    
+    def show_characteristics(self):
+        return f'{self.name},  cost = {self.cost}, resist = {self.resist}, hp = {self.hp}'
+
+class Heal_potion():
+    def __init__(self, name, hp, cost):
+        self.name = name
+        self.hp = hp
+        self.cost = cost
+
+class Heal_potion_small(Heal_potion):
+    def __init__(self, name = 'Heal potion small', hp = 25, cost = 20):
+        super().__init__(name, hp, cost)
+
+class Heal_potion_medium(Heal_potion):
+    def __init__(self, name = 'Heal potion medium', hp = 75, cost = 60):
+        super().__init__(name, hp, cost)
+
+class Heal_potion_big(Heal_potion): 
+    def __init__(self, name = 'Heal potion big', hp = 150, cost = 120):
+        super().__init__(name, hp, cost)
+
+class Room():
+    def __init__(self, enemy, money,):
+        self.enemy = enemy
+        self.money = money
+
+    def enter_room(self, player, time_nosleep):
+        print(f'{player.name} вошел в комнату\nв комнате {self.enemy.name}')
+        return self.fight(player, time_nosleep)
+
+    def fight(self, player: Player, time_nosleep):
+        
+        enemy = self.enemy
+        while player.hp > 0 and enemy.hp > 0:
+            player.deal_hit(enemy)
+            sleep(3 * (time_nosleep/100))
+            if enemy.hp > 0:
+                enemy.deal_hit(player)
+                sleep(3 * (time_nosleep/100))
+        if player.hp  <= 0:
+            return False
+        elif enemy.hp <= 0:
+            player.money += self.money
+            print(f'{enemy.name} повержен!\nВ комнате было {self.money} монет.\nТеперь у {player.name} имеет {player.money}')
+            sleep(3 * (time_nosleep/100))
+            player.inventory.append(enemy.weapon)
+            print(f'Еще теперь у вас есть {enemy.weapon.name} за {enemy.weapon.cost}.')
+            return True
+
+        
+
+class Dungeon():
+    def __init__(self, rooms, name):
+        self.rooms = rooms
+        self.name = name
+        self.counter = 0
+        self.passed = False
+    
+    def next_room(self, player, time_nosleep):
+        if self.counter < len(self.rooms):
+            is_alive = self.rooms[self.counter].enter_room(player, time_nosleep)
+            self.counter += 1
+            return is_alive
+        else:
+            self.passed = True
+
+class Trader():
+    def __init__(self, trader_inventory, trader_money, heal_potion_small, heal_potion_medium, heal_potion_big):
+        self.trader_inventory = trader_inventory
+        self.trader_money = trader_money
+        self.heal_potion_small = heal_potion_small
+        self.heal_potion_medium = heal_potion_medium
+        self.heal_potion_big = heal_potion_big
+
+
+    def trader_manager(self,player):
+        while True:
+            self.show_inventory()
+            action = input(f'1.продать\n2.купить\n3.выход\n')
+            if action == '1':
+                self.buy_item(player)
+            elif action == '2':
+                self.sell_item(player)
+            elif action == '3':
+                break
+            
+
+    def show_inventory(self):
+        number_cicle = 0
+        print(f'\nТорговец:')
+        for i in self.trader_inventory:
+            number_cicle += 1
+            print(f'{number_cicle}. {i.show_characteristics()}')
+        print(f'\n')
+
+    def sell_item(self, player:Player):
+        self.show_inventory()
+        position = int(input()) - 1
+        product = self.trader_inventory[position]
+        if self.trader_money >= product.cost:
+            self.trader_inventory.pop(position)
+            player.inventory.append(product)
+            player.money -= product.cost
+            self.trader_money += product.cost
+            self.show_inventory()
+            player.show_inventory()
+        else:
+            print('У вас не хватает денег')
+
+    def buy_item(self, player:Player):
+        player.show_inventory()
+        position = int(input()) - 1
+        product = player.inventory[position]
+        if self.trader_money >= product.cost:
+            player.inventory.pop(position)
+            self.trader_inventory.append(product)
+            self.trader_money -= product.cost
+            player.money += product.cost
+            self.show_inventory()
+            player.show_inventory()
+        else:
+            print('У торговца не хватает денег')
+            
+
+    def hpreg(self):
+        small = 0
+        medium = 0
+        big = 0
+        heals = [small, medium, big]
+        for i in self.trader_inventory:
---
+++ game.py
@@ -0,0 +1,156 @@
+from classes import Character, Player, Armor, Weapon, Room, Dungeon, Trader, Heal_potion_small, Heal_potion_medium, Heal_potion_big
+import time
+
+
+baby_dragon_s_skin = Armor("baby dragon's skin",25 ,10 , 50)
+none = Armor('none', 0, 0, 0)
+dragon_s_skin = Armor("dragon's skin",25,10,50)
+goblin_leather = Armor("Гоблинская кожаная броня", 10, 5, 20)
+goblin_chainmail = Armor("Гоблинская кольчуга", 20, 10, 40)
+hobgoblin_armor = Armor("Броня хобгоблина", 30, 15, 60)
+goblin_shaman_robes = Armor("Одеяния гоблина-шамана", 15, 20, 35)
+goblin_king_armor = Armor("Доспехи короля гоблинов", 50, 25, 100)
+skeleton_bones = Armor("Костяная броня скелета", 15, 5, 25)
+rusted_armor = Armor("Ржавые доспехи скелета", 25, 10, 45)
+skeleton_knight_armor = Armor("Доспехи скелета-рыцаря", 40, 15, 75)
+skeleton_mage_cloak = Armor("Плащ скелета-мага", 10, 25, 30)
+skeleton_lich_robes = Armor("Одеяния скелета-лича", 35, 40, 65)
+orc_hide = Armor("Орочья шкура", 30, 10, 55)
+orc_chain = Armor("Орочья кольчуга", 45, 15, 80)
+orc_plate = Armor("Орочий латный доспех", 60, 20, 110)
+orc_shaman_robes = Armor("Одеяния орка-шамана", 25, 35, 60)
+orc_warlord_armor = Armor("Доспехи орка-полководца", 75, 30, 140)
+
+baby_dragon_s_arm = Weapon("baby dragon's arm",25, 0)
+dragon_s_arm = Weapon("baby dragon's arm",25, 0)
+small_slime_weapon = Weapon('small slime weapon', 2, 0)
+medium_slime_weapon = Weapon('medium slime weapon', 20, 0)
+big_slime_weapon = Weapon('big_slime_weapon', 40, 0)
+giant_slime_weapon = Weapon('giant_slime_weapon', 120, 0)
+wooden_sword = Weapon('wooden sword', 15, 10)
+steal_sword = Weapon('steal sword', 50, 50)
+rotten_wood_sword = Weapon('"great" sword', 0.0001, 100)
+goblin_dagger = Weapon("Гоблинский кинжал", 15, 0)
+goblin_axe = Weapon("Гоблинский топор", 35, 0)
+goblin_bow = Weapon("Гоблинский лук", 25, 0)
+hobgoblin_club = Weapon("Дубина хобгоблина", 55, 0)
+goblin_king_sword = Weapon("Меч короля гоблинов", 80, 0)
+skeleton_bone = Weapon("Кость скелета", 10, 0)
+skeleton_sword = Weapon("Ржавый меч скелета", 25, 0)
+skeleton_archer_bow = Weapon("Лук скелета-лучника", 30, 0)
+skeleton_warrior_axe = Weapon("Топор скелета-воина", 45, 0)
+skeleton_mage_staff = Weapon("Посох скелета-мага", 60, 0)
+orc_club = Weapon("Дубина орка", 40, 0)
+orc_axe = Weapon("Топор орка", 65, 0)
+orc_berserker_axe = Weapon("Топор орка-берсерка", 85, 0)
+orc_shaman_staff = Weapon("Посох орка-шамана", 70, 0)
+orc_warlord_sword = Weapon("Меч орка-полководца", 110, 0)
+
+heal_potion_small = Heal_potion_small()
+heal_potion_medium = Heal_potion_medium()
+heal_potion_big = Heal_potion_big()
+
+#player==================================================================================================
+player = Player('player_1', 100, wooden_sword, 'control', none, 100, [rotten_wood_sword])
+
+#inventory========================================
+first_trder_inventory = [steal_sword, rotten_wood_sword, baby_dragon_s_skin, heal_potion_small, heal_potion_small, heal_potion_small,
+ heal_potion_medium, heal_potion_medium ,heal_potion_big]
+
+#trader========================================
+first_trder = Trader(first_trder_inventory, 999999999999999999999999999999)
+
+#slimes--------------------------------------------------------------------------------------------------
+small_slime = Character('small slime', 5, small_slime_weapon, 'enemy', none)
+medium_slime = Character('medium slime', 20, medium_slime_weapon, 'enemy', none)
+big_slime = Character('big slime', 70, medium_slime_weapon, 'enemy', none)
+giant_slime = Character('giant slime', 30, giant_slime_weapon, 'enemy', none)
+goblin_scout = Character("Гоблин-разведчик", 25, goblin_dagger, 'enemy', goblin_leather)
+goblin_warrior = Character("Гоблин-воин", 40, goblin_axe, 'enemy', goblin_chainmail)
+goblin_archer = Character("Гоблин-лучник", 35, goblin_bow, 'enemy', goblin_leather)
+hobgoblin = Character("Хобгоблин", 80, hobgoblin_club, 'enemy', hobgoblin_armor)
+goblin_king = Character("Король гоблинов", 150, goblin_king_sword, 'enemy', goblin_king_armor)
+skeleton = Character("Скелет", 30, skeleton_bone, 'enemy', skeleton_bones)
+skeleton_warrior = Character("Скелет-воин", 60, skeleton_sword, 'enemy', rusted_armor)
+skeleton_archer = Character("Скелет-лучник", 50, skeleton_archer_bow, 'enemy', skeleton_bones)
+skeleton_knight = Character("Скелет-рыцарь", 90, skeleton_warrior_axe, 'enemy', skeleton_knight_armor)
+skeleton_mage = Character("Скелет-маг", 70, skeleton_mage_staff, 'enemy', skeleton_mage_cloak)
+orc_grunt = Character("Орк-пехотинец", 65, orc_club, 'enemy', orc_hide)
+orc_warrior = Character("Орк-воин", 100, orc_axe, 'enemy', orc_chain)
+orc_berserker = Character("Орк-берсерк", 120, orc_berserker_axe, 'enemy', orc_hide)
+orc_shaman = Character("Орк-шаман", 85, orc_shaman_staff, 'enemy', orc_shaman_robes)
+orc_warlord = Character("Орк-полководец", 180, orc_warlord_sword, 'enemy', orc_warlord_armor)
+
+#dragons-------------------------------------------------------------------------------------------------
+baby_dragon = Character('baby_dragon', 100, baby_dragon_s_arm, 'enemy', baby_dragon_s_skin)
+dragon = Character('dragon', 125, dragon_s_arm, 'enemy', dragon_s_skin)
+
+#rooms===================================================================================================
+room1 = Room(small_slime, 5)
+room2 = Room(medium_slime, 10)
+room3 = Room(big_slime, 50)
+room4 = Room(baby_dragon, 50)
+room5 = Room(dragon, 75)
+goblin_room1 = Room(goblin_scout, 15)
+goblin_room2 = Room(goblin_warrior, 25)
+goblin_room3 = Room(goblin_archer, 20)
+goblin_room4 = Room(hobgoblin, 50)
+goblin_room5 = Room(goblin_king, 100)
+skeleton_room1 = Room(skeleton, 20)
+skeleton_room2 = Room(skeleton_warrior, 35)
+skeleton_room3 = Room(skeleton_archer, 30)
+skeleton_room4 = Room(skeleton_knight, 60)
+skeleton_room5 = Room(skeleton_mage, 45)
+orc_room1 = Room(orc_grunt, 40)
+orc_room2 = Room(orc_warrior, 65)
+orc_room3 = Room(orc_berserker, 75)
+orc_room4 = Room(orc_shaman, 55)
+orc_room5 = Room(orc_warlord, 120)
+
+slime_rooms_list = [room1, room2, room3]
+dragon_rooms_list = [room4]
+goblin_rooms_list = [goblin_room1, goblin_room2, goblin_room3, goblin_room4, goblin_room5]
+skeleton_rooms_list = [skeleton_room1, skeleton_room2, skeleton_room3, skeleton_room4, skeleton_room5]
+orc_rooms_list = [orc_room1, orc_room2, orc_room3, orc_room4, orc_room5]
+
+slime_dungen = Dungeon(slime_rooms_list, 'подземелье слаймов')
+dragon_dungen = Dungeon(dragon_rooms_list, 'подземелье драконов')
+goblin_dungeon = Dungeon(goblin_rooms_list, 'Подземелье гоблинов')
+skeleton_dungeon = Dungeon(skeleton_rooms_list, 'Подземелье скелетов')
+orc_dungeon = Dungeon(orc_rooms_list, 'Подземелье орков')
+
+def time_nosleeps(time_nosleep):
+    print(f'Сейчас {time_nosleep} %')
+    time_nosleep = int(input(f'Задайть процент для времени ожидания:'))
+    return(time_nosleep)
+
+choose = 0
+all_dungeon = [slime_dungen, dragon_dungen, goblin_dungeon, skeleton_dungeon, orc_dungeon]
+print(f'Вы провалились в подземелье')
+time_nosleep = int(input(f'Задайте процент для времени ожидания: '))
+time.sleep(3*time_nosleep)
+for dungen in all_dungeon:
+    print(f'Вы, вошли в подземелье {dungen.name}')
+    while dungen.passed == False:
+        print(f'1.торговец\n2.инвентарь\n3.в бой!\n4.задать процент для времени ожидания')
+        choose = int(input('1, 2, 3, 4? '))
+        if choose == 1:
+            first_trder.trader_manager(player)
+        elif choose == 2:
+            player.player_manager()
+        elif choose == 3:
+            is_alive = dungen.next_room(player, time_nosleep)
+            if is_alive == False:
+                print(f'К сожелению Вы умерли, но вы можете снова пройти тот же путь...')
+                break
+        elif choose == 4:
+            time_nosleep = time_nosleeps(time_nosleep)
+        if dungen.passed == True:
+            break
+        else :
+            print('непонял')
+    print(f'Вы прошли уровень {dungen.name},\nтак держать!\n вы нашли 100 монет')
+    player.money += 100
+    print(f'теперь у вас {player.money}')
+if is_alive == True:
+    print('Поздрвляю, вы прошли подземелье и выбрались на поверхность!')
